d8a4d9c44c79d1c2eedbff2631a08f69ed1a6391
Razor202020 <jordanjlum@gmail.com>
2025-03-10 14:02:29 -0700

    ad

--- project-delta/player/player.gd
+++ project-delta/player/player.gd
@@ -1,4 +1,6 @@
 extends CharacterBody2D
-func _process(delta):
-	var direction = Input.get_vector("Left","Right","Up","Down")
-	position += direction * 500 * delta
+func _process(_delta):
+	
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * 500
+	move_and_slide()
