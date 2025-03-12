db51628584c66a6203aad3c7a6ddbe8d6931aacf
Razor202020 <jordanjlum@gmail.com>
2025-03-12 15:01:38 -0700

    ssf

--- project-delta/player/player.gd
+++ project-delta/player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 func _process(_delta):
-	var direction = Vector2.RIGHT
-	velocity = direction * 50
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * 1
 	move_and_slide()
-	#Input.get_vector("Left", "Right", "Up", "Down")a
+	Input.get_vector("Left", "Right", "Up", "Down")
