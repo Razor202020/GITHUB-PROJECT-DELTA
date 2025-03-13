b49a933f9436d495333b431fada8243d7d0460f8
Razor202020 <jordanjlum@gmail.com>
2025-03-13 14:58:32 -0700

    ddasd

--- project-delta/Enemy/enemy.gd
+++ project-delta/Enemy/enemy.gd
@@ -2,5 +2,4 @@ extends CharacterBody2D
 func _process(_delta):
 	var direction = Vector2.LEFT
 	velocity = direction * 50
-	
 	move_and_slide()
--- project-delta/player/player.gd
+++ project-delta/player/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
-func _process(_delta):
-	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * 1
+
+const SPEED = 100
+
+func _process(_delta) -> void:
+	var direction = Input.get_vector("Left", "Right", "Up", "Down")
+	velocity = direction * SPEED
 	move_and_slide()
-	Input.get_vector("Left", "Right", "Up", "Down")
