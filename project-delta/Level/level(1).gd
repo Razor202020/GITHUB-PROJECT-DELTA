1b0bbf895f2c95a7ccd3e43ecc6247d8c1450754
Razor202020 <jordanjlum@gmail.com>
2025-04-09 15:00:23 -0700

    dsa

--- project-delta/Level/level(1).gd
+++ project-delta/Level/level(1).gd
@@ -10,3 +10,7 @@ func _process(_delta: float) -> void:
 	if Input.is_action_pressed("interact") and flag == true:
 		flag = false
 		get_tree().change_scene_to_file("res://Level/level(2).tscn")
+
+
+func _on_player_laser():
+	print("laser form level")
--- project-delta/player/player.gd
+++ project-delta/player/player.gd
@@ -1,9 +1,13 @@
+#2:49:38
 extends CharacterBody2D
-
+signal laser
+var can_laser: bool = true
 const SPEED = 110
 var walk = false
 var flag_r = true
-func _process(_delta) -> void:
+var target = false
+var flag_movement_aim = false
+func _process(_delta):
 	var direction = Input.get_vector("Left", "Right", "Up", "Down")
 	velocity = direction * SPEED
 	move_and_slide()
@@ -21,13 +25,27 @@ func _process(_delta) -> void:
 	if Input.is_action_just_released("Right"):
 		walk = false
 		$Sprite2D.play("IDLE")
-	if Input.is_action_pressed("PrimaryAction"):
+	if Input.is_action_pressed("PrimaryAction") and can_laser and walk == true and target == true:
 		flag_r = false
+		can_laser = false
 		$Sprite2D.play("gun-walk")	
+		$Timer.start()
+		laser.emit()
 	if Input.is_action_just_released("PrimaryAction"):
+		$Sprite2D.play("IDLE")
 		flag_r = true
-		$Sprite2D.play("IDLE")
-	if Input.is_action_pressed("PrimaryAction") and walk == false:
-		$Sprite2D.play("gun")	
+	if Input.is_action_pressed("PrimaryAction") and walk == false and can_laser and target == true:
+		$Sprite2D.play("gun")
+		can_laser = false
+		$Timer.start()
+		laser.emit()
 	if Input.is_action_just_released("PrimaryAction"):
 		$Sprite2D.play("IDLE")
+	if Input.is_action_just_pressed("Aim"):
+		$Sprite2D.play("gun")
+		print("aiming")
+	if Input.is_action_just_released("Aim"):
+		$Sprite2D.play("IDLE")
+		target = true
+func _on_timer_timeout():
+	can_laser = true
