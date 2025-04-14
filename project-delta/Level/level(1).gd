16e052a85f656dd1e2ed015e66f8f7a43dc687d3
Razor202020 <jordanjlum@gmail.com>
2025-04-14 13:30:20 -0700

    v

--- project-delta/Level/level(1).gd
+++ project-delta/Level/level(1).gd
@@ -1,5 +1,4 @@
 extends Node2D
-
 var movement_flag = false
 var flag = false
 func _on_area_2d_body_entered(_body: Node) -> void:
@@ -9,10 +8,12 @@ func _process(_delta: float) -> void:
 	get_global_mouse_position()
 	if Input.is_action_pressed("interact") and flag == true:
 		flag = false
-		movement_flag = true
 		get_tree().change_scene_to_file("res://Level/secret.tscn")
+		movement_flag = true
 	if movement_flag == true:
-		move_local_x(310)
+		print("bruh this is a test")
+		position.x = 691
+		movement_flag = false
 
 func _on_player_laser():
 	print("laser form level")
--- project-delta/Level/secret.gd
+++ project-delta/Level/secret.gd
@@ -1,7 +1,4 @@
 extends Node2D
-
-
-
 var flag = false
 func _on_area_2d_body_entered(_body: Node) -> void:
 	print("second test you are in the rock demention")
@@ -10,4 +7,3 @@ func _process(_delta: float) -> void:
 	if Input.is_action_pressed("interact") and flag == true:
 		flag = false
 		get_tree().change_scene_to_file("res://Level/level(1).tscn")
-		move_local_x(310)
