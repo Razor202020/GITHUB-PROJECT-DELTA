95ad66eaddb5d84cf51cde265d893b1e87b36654
Razor202020 <jordanjlum@gmail.com>
2025-05-02 13:33:18 -0700

    jk

--- project-delta/player/real_laser.gd
+++ project-delta/player/real_laser.gd
@@ -2,18 +2,12 @@ extends Node2D
 var flag = false
 var tflag = false
 var Enemy_In_Attack_Range = false
-@export var speed: int = 1000
+@export var speed: int = 10000
 var direction: Vector2 = Vector2.RIGHT
-func _process(delta):
-	#if $Sprite2D.flip_h == true:
-		#direction = Vector2.LEFT
-		#print("from REAL LASER")
-	#if $Sprite2D.flip_h == false:
-		#direction = Vector2.RIGHT
-	#$Sprite2D.player_direction
-	#player_direction
+func _physics_process(delta: float) -> void:
 	position += direction * delta * speed * -1 if Globals.player_direction == 'left' else direction * delta * speed * 1
-
+func player():
+	pass
 
 func _on_body_entered(body):
 	if body.has_method("Enemy"):
@@ -23,3 +17,5 @@ func _on_body_entered(body):
 func _on_body_exited(body):
 	if body.has_method("Enemy"):
 		Enemy_In_Attack_Range = false
+func enemy_attack():
+	print("player")
