4e9460d6d9f6c0c7092ad756d6f7e6c4b7395068
Razor202020 <jordanjlum@gmail.com>
2025-05-29 14:19:59 -0700

    f

--- project-delta/Level/level(1)(1).gd
+++ project-delta/Level/level(1)(1).gd
@@ -1,8 +1,9 @@
 extends Node2D
 var laser_scene: PackedScene = preload("res://player/real_laser.tscn")
 var flag = false
-func _on_area_2d_body_entered(_body):
-	flag = true	
+func _on_area_2d_body_entered(body):
+	if body.has_method("player"):
+		flag = true	
 func _process(_delta):
 	get_global_mouse_position()
 	if Input.is_action_pressed("interact") and flag == true:
