a4f48ebbda4f91be2811448979fc4242eef7c8da
Razor202020 <jordanjlum@gmail.com>
2025-06-02 14:39:23 -0700

    sigm

--- project-delta/Level/level(1)(4).gd
+++ project-delta/Level/level(1)(4).gd
@@ -1,16 +1,15 @@
 extends Node2D
 var laser_scene: PackedScene = preload("res://player/real_laser.tscn")
 var flag = false
-func _on_area_2d_body_entered(_body):
-	print("level3")
-	flag = true	
+func _on_area_2d_body_entered(body):
+	if body.has_method("player"):
+		flag = true	
 func _process(_delta):
 	get_global_mouse_position()
 	if Input.is_action_pressed("interact") and flag == true:
 		flag = false
-		get_tree().change_scene_to_file("res://Level/level(1)(4).tscn")
+		get_tree().change_scene_to_file("res://Level/level(1)(2).tscn")
 func _on_player_laser(pos):
-	print("laser form level")
 	var laser = laser_scene.instantiate()
 	laser.position = pos
 	$projectiles.add_child(laser)
