cf093d3865952fc47f18279e1ed1784663c562b1
Razor202020 <jordanjlum@gmail.com>
2025-06-17 14:51:12 -0700

    h

--- project-delta/budi.gd
+++ project-delta/budi.gd
@@ -2,8 +2,15 @@ extends AnimatedSprite2D
 var flag = true
 func _ready() -> void:
 	$".".play("bootup")
+	
+	while true:
+		$".".play("yap")
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("close"):
 		queue_free()
 	
 		
+
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
