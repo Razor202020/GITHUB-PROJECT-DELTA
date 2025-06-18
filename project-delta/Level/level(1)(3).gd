141713a40cafa5f895763c3056838f6ea5937045
Razor202020 <jordanjlum@gmail.com>
2025-06-18 14:36:27 -0700

    fd

--- project-delta/Level/level(1)(3).gd
+++ project-delta/Level/level(1)(3).gd
@@ -1,18 +1,19 @@
 extends Node2D
 var laser_scene: PackedScene = preload("res://player/real_laser.tscn")
 var flag = false
-func _on_area_2d_body_entered(_body):
-	print("level2")
-	flag = true	
+func _on_area_2d_body_entered(body):
+	if body.has_method("player"):
+		flag = true	
 func _process(_delta):
 	get_global_mouse_position()
 	if Input.is_action_pressed("interact") and flag == true:
 		flag = false
-		get_tree().change_scene_to_file("res://Level/secret.tscn")
+		get_tree().change_scene_to_file("res://Level/level(1)(2).tscn")
 func _on_player_laser(pos):
-	print("laser form level")
 	var laser = laser_scene.instantiate()
 	laser.position = pos
 	$projectiles.add_child(laser)
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	flag = false
+func _ready() -> void:
+	DialogueManager.show_example_dialogue_balloon(load("res://dialogue/room2.dialogue"), "start")
--- project-delta/budi.gd
+++ project-delta/budi.gd
@@ -2,8 +2,10 @@ extends AnimatedSprite2D
 var flag = true
 func _ready() -> void:
 	$".".play("bootup")
+	$Timer.start()
 func _process(delta: float) -> void:
+	
 	if Input.is_action_just_pressed("close"):
 		queue_free()
-	
-		
+func _on_timer_timeout() -> void:
+	$".".play("yap")
